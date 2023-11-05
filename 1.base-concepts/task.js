9437df42d7c5eb18e88b6aa459b75d8da3936d2e
Grigory <weissman1979@gmail.com>
2023-11-05 15:20:51 +0400

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,26 +3,25 @@
 function solveEquation(a, b, c) {
   let arr = [];
 
-	let discr = b ** 2 - 4 * a * c;
-  return arr;	
-	if (discr > 0) {
-		arr.push((-b + Math.sqrt(discr)) / (2 * a));
-		arr.push((-b - Math.sqrt(discr)) / (2 * a));
-	} else if (discr === 0) {
+	let discriminant = b ** 2 - 4 * a * c;
+	
+	if (discriminant > 0) {
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+	} else if (discriminant === 0) {
 		arr.push(-b / (2 * a));
-	} else {
 	}
 
 	return arr;
 }
 
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {	function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-	let s = amount - contribution;
-	let p = (percent * 0.01) / 12;
-	let payment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let credit = amount - contribution;
+	let bid = (percent * 0.01) / 12;
+	let payment = credit * (bid + (bid / (((1 + bid) ** countMonths) - 1)));
 	let sum = payment * countMonths;
 
 	return +sum.toFixed(2);
-}	}
+}
