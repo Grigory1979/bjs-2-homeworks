dac0729de853a93e8bce982deb62fbb29a737417
Grigory <weissman1979@gmail.com>
2023-11-18 16:16:32 +0400

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,84 +1,28 @@
-class PrintEditionItem {
-    #state;
-    constructor (name, releaseDate, pagesCount) {
+class Student {
+    constructor(name){
         this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.#state = 100;
-        this.type = null; 
+        this.marks = {};
     }
-    set state(state) {
-        if (state < 0) {this.#state = 0}
-        else if (state > 100) {this.#state = 100}
-        else this.#state = state;
-    }
-    get state() {
-        return this.#state;
-    }
-    fix() {
-        this.state = this.state * 1.5;
-    }
-}
-
-class Magazine extends PrintEditionItem {
-    constructor (name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.type = "magazine";
-    }
-}
-
-class Book extends PrintEditionItem {
-    constructor (author, name, releaseDate, pagesCount) {
-        super(name, releaseDate, pagesCount);
-        this.author = author;
-        this.type = "book";
-    }
-}
-
-class NovelBook extends Book {
-    constructor (author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount);
-        this.type = "novel";
-    }
-}
-
-class FantasticBook extends Book {
-    constructor (author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount);
-        this.type = "fantastic";
-    }
-}
-
-class DetectiveBook extends Book {
-    constructor (author, name, releaseDate, pagesCount) {
-        super(author, name, releaseDate, pagesCount);
-        this.type = "detective";
-    }
-}
-
-class Library {
-    constructor (name) {
-        this.name = name;
-        this.books = [];
-    }
-    addBook(book) {
-        if (book.state > 30) {
-            this.books.push(book);
+    addMark(mark, subject) {
+        if (mark < 2 || mark > 5) { return; }
+        if (!this.marks.hasOwnProperty(subject)) {
+            this.marks[subject] = [];
         }
+        this.marks[subject].push(mark);
     }
-    findBookBy(typeString, value) {
-        for (let i = 0; i < this.books.length; i++) {
-            if (this.books[i][typeString] === value) {
-                return this.books[i];
-            }
+    getAverageBySubject(subject) {
+        if (!this.marks.hasOwnProperty(subject)) {
+            return 0;
         }
-        return null;
+        return this.marks[subject].reduce((acc, mark, idx, arr) => acc + mark / arr.length, 0); 
     }
-    giveBookByName(bookName) {
-        let foundBook = this.findBookBy("name", bookName);
-        if (foundBook !== null) {
-            this.books = this.books.filter((book) => book !== foundBook)
+    getAverage() {
+        let keys =  Object.keys(this.marks);
+        if (keys.length === 0) { return 0;}
+        let avgResult = 0;
+        for (let i = 0; i < keys.length; i++) {
+            avgResult += this.marks[keys[i]].reduce((acc, mark, idx, arr) => acc + mark / arr.length, 0); 
         }
-        return foundBook;
+        return avgResult / keys.length;
     }
 }
