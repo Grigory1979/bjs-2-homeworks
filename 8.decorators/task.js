0dff943a3714fe8ce11bb7e50fa42e3f2324705a
Grigory <weissman1979@gmail.com>
2023-11-25 18:24:16 +0400

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,46 @@
 //Задача № 1
+
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+	const maxCacheValuesCount = 5;
+	return (...args) => {
+		const hash = args.join(',');
+		for (let i = 0; cache.length > i; i++) {
+			if (cache[i].hash === hash) {
+				return 'Из кэша: ' + cache[i].value;
+			}
+		}
+		const result = func(...args);
+		cache.push({ hash: hash, value: result });
+		const actualCacheValuesCount = Object.keys(cache).length;
+
+		if (actualCacheValuesCount > maxCacheValuesCount) {
+			cache.shift();
+		}
+
+		return 'Вычисляем: ' + result;
+	};
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	function wrapper(...args) {
+		wrapper.allCount++;
+
+		if (timeoutId === null) {
+			func(...args);
+			wrapper.count++;
+		} else {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			func.call(this, ...args);
+			wrapper.count++;
+		}, delay);
+	}
+	return wrapper;
 }
